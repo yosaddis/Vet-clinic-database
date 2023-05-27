5c3417b72f46eb0aebb03a0d54eb57d5021320fe
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-05-27 16:08:48 +0300

    Update schema.sql to add vet table and join tables

--- schema.sql
+++ schema.sql
@@ -43,3 +43,23 @@ ADDs
   CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id) ON DELETE
 SET
   NULL;
+
+CREATE TABLE IF NOT EXISTS vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(55),
+  age INT,
+  date_of_graduation DATE
+);
+
+CREATE TABLE IF NOT EXISTS specializations(
+  species_id INT REFERENCES species(id),
+  vet_id INT REFERENCES vets(id),
+  PRIMARY KEY (species_id, vet_id)
+);
+
+CREATE TABLE IF NOT EXISTS visits(
+  animal_id INT REFERENCES animals(id),
+  vet_id INT REFERENCES vets(id),
+  date DATE NOT NULL,
+  PRIMARY KEY (animal_id, vet_id, date)
+);
